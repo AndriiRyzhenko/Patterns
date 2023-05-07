491de455e72e59acddff699ab9542ddaafdfcd04
Andrii Ryzhenko <andrii.s.ryzhenko@globallogic.com>
2023-05-07 13:31:24 +0300

    7 and 8 labs were added

---
+++ Lab_8/Program.cs
@@ -0,0 +1,24 @@
+﻿using Lab_8.Builders;
+using Lab_8.Interfaces;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IQueryBuilder queryBuilder = new PostgreSqlQueryBuilder();
+        string postgreSqlQuery = queryBuilder.Select("Products").Where("type = Phone").Limit(50).GetSql();
+        Console.WriteLine(postgreSqlQuery);
+
+        queryBuilder = new MySqlQueryBuilder();
+        string mySqlQuery = queryBuilder.Select("Users").Where("age = 30").Limit(10).GetSql();
+        Console.WriteLine(mySqlQuery);
+
+
+        /*
+        Console shows next:
+
+        SELECT * FROM Products WHERE type = Phone LIMIT 50
+        SELECT * FROM Users WHERE age = 30 LIMIT 10
+        */
+    }
+}
